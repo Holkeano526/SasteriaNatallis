a9d6556d53585764918758f9d13b7577108679c6
Holkeano526 <anthonyelpapupro@gmail.com>
2023-07-08 11:26:21 -0500

    primer commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.scrollviewizquierda',{
+        duration:1500,
+        origin: 'botton',
+        distance:'-100px'
+    })
+
+    sr.reveal('.scrollviewderecha',{
+        duration:1500,
+        origin: 'botton',
+        distance:'100px'
+    })
+
+    sr.reveal('.scrollviewheader',{
+        duration:1500,
+        origin: 'top',
+        distance:'100px'
+    })
+
+    sr.reveal('.scrollimagenes',{
+        duration:1500,
+        origin: 'top',
+        distance:'100px'
+    })
+
+
+    
